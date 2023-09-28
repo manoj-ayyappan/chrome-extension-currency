0f0a5180c007d559be082ca56d9054180edd1ca7
mayyapp <mayyapp@ncsu.edu>
2023-09-27 23:06:54 -0400

    Fixed bugs

--- exchange.js
+++ exchange.js
@@ -1,29 +1,32 @@
-const amount = document.getElementById('amount');
-const currency = document.getElementById('currency');
-const convert = document.getElementById('convert');
-const result = document.getElementById('result');
+document.addEventListener('DOMContentLoaded', function() {
+    const amount = document.getElementById('amount');
+    const currency = document.getElementById('currency');
+    const convert = document.getElementById('convert');
+    const result = document.getElementById('result');
 
-const apiKey = "rFGF0fOmY6zjWST41JjnSw==oynlyZottlpu7VMH"
-const apiUrl = "https://api.api-ninjas.com/v1/convertcurrency?have=USD&want="
+    const apiKey = "rFGF0fOmY6zjWST41JjnSw==oynlyZottlpu7VMH"
+    const apiUrl = "https://api.api-ninjas.com/v1/exchangerate?pair=USD_"
 
-convert.addEventListener('click', () => {
-    const amountTotal = amount.value;
-    const currencyTotal = currency.value;
-    const url = apiUrl + currencyTotal;
+    convert.addEventListener('click', () => {
+        const amountTotal = amount.value;
+        const currencyTotal = currency.value;
+        const url = apiUrl + currencyTotal;
 
-    fetch(url, {
-        headers: {
-            'X-API-KEY': apiKey
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        const rate = data.exchange_rate;
-        const resultPrice = amountTotal * rate;
-        result.innerHTML = `${amount} ${currency} = ${resultPrice.toFixed(2)} USD`;
-    })
-    .catch(error => {
-        console.error('Request failed ', error);
-        result.innerHTML = 'An error occured please try again later'
+        fetch(url, {
+            headers: {
+                'X-API-KEY': apiKey
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            const rate = data.exchange_rate;
+            const resultPrice = amountTotal * rate;
+            result.innerHTML = `${amountTotal} USD = ${resultPrice.toFixed(2)} ${currencyTotal}`;
+        })
+        .catch(error => {
+            console.error('Request failed ', error);
+            result.innerHTML = 'An error occured please try again later'
+        })
     })
 })
+
